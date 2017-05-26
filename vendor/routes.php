8e4bee726a95409ee79bab7bea670e6bf19f0a00
SkebloW <skeblow@gmail.com>
2017-05-26 16:01:44 +0200

    natankuj

--- vendor/routes.php
+++ vendor/routes.php
@@ -21,7 +21,26 @@ $app->group('/v1', function () use ($app) {
                 ->execute()
             ;
 
+            $now = (new DateTime())->format('Y-m-d H:i:s');
+
             while ($user = $stmt->fetchObject()) {
+
+                $stmt2 = $db->select(['OSID'])
+                    ->count('*', 'c')
+                    ->from('Dochazka')
+                    ->where('Prichod', '>', $now)
+                    ->where('OSID','=', $user->ID)
+                    ->execute()
+                ;
+
+                $obj = $stmt2->fetchObject();
+
+                if ($obj->c) {
+                    $user->status = 3;
+                } else {
+                    $user->status = $user->online + 1;
+                }
+
                 if ($whoof) {
                     if (in_array($user->ID, [15, 16, 17, 18, 22])) {
                         $return[] = $user;
@@ -100,17 +119,72 @@ $app->group('/v1', function () use ($app) {
                         ->into('Dochazka')
                         ->execute();
                 }
-
                 $db->commit();
 
+                return $response->withStatus(204);
             } catch( Exception $ex ) {
                 $db->rollBack();
                 return $response->withStatus(400)
                     ->withJson(['error' => 'Some dog is in.']);
             }
+        });
+
+        $app->post('/natankuj', function (\Slim\Http\Request $request, \Slim\Http\Response $response) {
+            $body = $request->getParsedBody();
+
+            if (!is_array($body['dogs'])) {
+                return $response->withStatus(400)
+                    ->withJson(['error' => 'Body has no dogs.']);
+            }
+
+            /** @var Slim\PDO\Database $db */
+            $db = $this->db;
+
+            $now = (new DateTime())->format('Y-m-d H:i:s');
+            $after = ((new DateTime())->add(new DateInterval('PT30M')))->format('Y-m-d H:i:s');
+
+            try {
+
+                foreach ($body['dogs'] as $dog) {
+
+                    $stmt = $db->select()
+                        ->count('*', 'c')
+                        ->from('Dochazka')
+                        ->where('Odchod', '=', '0000-00-00 00:00:00')
+                        ->where('OSID', '=', $dog)
+                        ->execute()
+                    ;
+
+                    $obj = $stmt->fetchObject();
+
+                    if (!$obj->c) {
+                        throw new Exception();
+                    }
 
 
-            return $response->withStatus(204);
+                    $db->update(['Odchod' => $now])
+                        ->table('Dochazka')
+                        ->where('Odchod', '=' , '\'0000-00-00 00:00:00\'')
+                        ->where('OSID', '=', $dog)
+                        ->execute();
+
+                    $db->insert([
+                        'OSID' => $dog,
+                        'Prichod' => $after,
+                        'Odchod' => '\'0000-00-00 00:00:00\'',
+                    ])
+                        ->into('Dochazka')
+                        ->execute();
+
+                }
+
+
+                return $response->withStatus(204);
+            } catch( Exception $ex ) {
+                return $response->withStatus(400)
+                    ->withJson(['error' => 'Some dog is in.']);
+            }
+
         });
     });
 
